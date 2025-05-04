81f3b85bb7e826cb2dd513f87a8a439f036b39dc
SooryaPandian <sooryapandian37@gmail.com>
2025-05-04 16:05:20 +0530

    added 2FA

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const { register, login, sendOtp, verifyOtp, approveExpert } = require("../controllers/authController");
+const {
+  register,
+  login,
+  sendOtp,
+  verifyOtp,
+  approveExpert,
+  validateTwoFactor,
+} = require("../controllers/authController");
 const { authenticate } = require("../middleware/authMiddleware");
 
 const router = express.Router();
@@ -9,6 +16,6 @@ router.post("/login", login);
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
 router.post("/approve-expert", authenticate, approveExpert); // Requires authentication
+router.post("/validate-2fa", authenticate, validateTwoFactor);
 
 module.exports = router;
-        
