d758319676cee458c634dabd37bb26e84f05a1ea
Sivasri R <sivasrir.22cse@kongu.edu>
2025-05-05 15:25:57 +0530

    Layouts added

---
+++ client/src/pages/Complaints.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Complaints = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-2">All Complaints</h2>
+      <p>This section will display submitted complaints.</p>
+    </div>
+  );
+};
+
+export default Complaints;
